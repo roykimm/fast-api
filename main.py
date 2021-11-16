3ff5fdb6209e2f0cdc3ee7a0d2bdb221da23fa09
roykimm <roykimmyunghoon@gmail.com>
2021-11-17 00:00:39 +0900

    1 houre

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {'data': {'name': 'Gobella'}}
+@app.get("/blog")
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blogs blogs from database '}
+    else:
+        return {'data': f'{limit} blogs blogs from database '}
 
 
-@app.get("/about")
-def about():
-    return {'data': 'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id, limit=10):
+
+    return {'data': {'1', '2'}}
+
+
+@app.post('/blog')
+def create_blog():
+    return {'data': "Blog is created"}
